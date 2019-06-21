b91c16627a49bab8b5436fd01f3e630ff00c5b33
Charlan <mattergremio2@gmail.com>
2019-06-21 10:22:16 -0300

    a

--- application/views/cadManutencaoView.php
+++ application/views/cadManutencaoView.php
@@ -1,9 +1,13 @@
+<link href="<?php echo base_url()?>assets/vendor/datepicker/css/daterangepicker.css" rel="stylesheet">
 <div class="container">
     <div class="card card-register mx-auto mt-5">
       <div class="card-header">Registrar uma Manutenção</div>
       <div class="card-body">
-        <form method="POST" action="<?php echo base_url();?>index.php/ServicoController/CadastrarServico">
-            <!-- Select Basic -->
+        <form method="get" action="<?php echo base_url();?>index.php/ServicoController/CadastrarServico">
+            <div class="form-group">
+                <label class="control-label" for="date">Data estimada</label>
+                <input type="text" class="daterange form-control"  value="" name="datas" />
+            </div>
             <div class="form-group">
             <label class="col-md-12 control-label" for="selectbasic">Empresa</label>
                 <div class="col-md-12 ">
@@ -17,7 +21,6 @@
                 </div>
             </div>
 
-            <!-- Select Basic -->
             <div class="form-group">
             <label class="col-md-12 control-label" for="selectbasic">Veiculo</label>
                 <div class="col-md-12 ">
@@ -31,8 +34,6 @@
                 </div>
             </div>
 
-
-            <!-- Select Basic -->
             <div class="form-group">
             <label class="col-md-12 control-label" for="selectbasic">Serviço</label>
                 <div class="col-md-12 ">
@@ -45,7 +46,6 @@
                     </select>
                 </div>
             </div>
-            
             <input class="btn btn-primary btn-block" type="submit" value="Enviar" name="input" class="text-center">   
         </form>
        <div class="text-center">
@@ -53,3 +53,13 @@
     </div>
   </div>
 </div>
+
+<script>
+    $(function() {           
+        $('.daterange').daterangepicker();  
+    });
+
+</script>
+
+<script src="<?php echo base_url()?>assets/vendor/datepicker/js/moment.min.js"></script>
+<script src="<?php echo base_url()?>assets/vendor/datepicker/js/daterangepicker.js"></script>
--- application/views/templates/headerViewAdmin.php
+++ application/views/templates/headerViewAdmin.php
@@ -19,9 +19,11 @@
 
   <!-- Page level plugin CSS-->
   <link href="<?php echo base_url()?>assets/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+  
 
   <!-- Custom styles for this template-->
   <link href="<?php echo base_url()?>assets/css/sb-admin.css" rel="stylesheet"> 
+  <link href="<?php echo base_url()?>assets/vendor/datepicker/css/daterangepicker.css" rel="stylesheet">
 
 </head>
 <body id="page-top">
