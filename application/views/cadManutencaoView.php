a7f70bbacdb3f6530129965a8a762a003b2c97fa
Charlan <mattergremio2@gmail.com>
2019-06-21 10:47:49 -0300

    Update cadManutencaoView.php

--- application/views/cadManutencaoView.php
+++ application/views/cadManutencaoView.php
@@ -56,8 +56,60 @@
 
 <script>
     $(function() {           
-        $('.daterange').daterangepicker();  
+        $('.daterange').daterangepicker({
+            "autoApply": true,
+            "maxSpan": {
+                "days": 20
+            },
+            "singleDatePicker": false,
+            "locale": {
+                "format": "MM/DD/YYYY",
+                "separator": " - ",
+                "applyLabel": "Aplicar",
+                "cancelLabel": "Cancelar",
+                "fromLabel": "Para",
+                "toLabel": "De",
+                "customRangeLabel": "Customizar",
+                "weekLabel": "Sem",
+                "daysOfWeek": [
+                    "Dom",
+                    "Seg",
+                    "Ter",
+                    "Qua",
+                    "Qui",
+                    "Sex",
+                    "Sab"
+                ],
+                "monthNames": [
+                    "Janeiro",
+                    "Fevereiro",
+                    "Março",
+                    "Abril",
+                    "Maio",
+                    "Junho",
+                    "Julho",
+                    "Agosto",
+                    "Setembro",
+                    "Outubro",
+                    "Novembro",
+                    "Dezembro"
+                ],
+                "firstDay": 1
+            },
+            "startDate": "06/15/2019",
+            "endDate": "06/21/2019"
+        }, function(start, end, label) {
+        console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
+        });
     });
+
+    function getDataAtual() {
+        var d = new Date();
+        var day = addZero(d.getDate());
+        var month = addZero(d.getMonth()+1);
+        var year = addZero(d.getFullYear());
+        return day + ". " + month + ". " + year + " (" + h + ":" + m + ")";
+    }
 
 </script>
 
